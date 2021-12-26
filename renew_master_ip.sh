a80611394a6cedcb66ffcb832e6f435dbfd6e10e
FONIA <3069193298@qq.com>
2021-12-26 17:15:12 +0800

    Update renew_master_ip.sh

--- renew_master_ip.sh
+++ renew_master_ip.sh
@@ -9,21 +9,98 @@ o_ip=$1
 n_ip=$2
 t_stm=`date "+%s"`
 
-if [ ! -f "/etc/kubernetes/manifests/etcd.yaml" ];then
-    echo '\E[1;32m [v] /etc/kubernetes/manifests/etcd.yaml文件不存在 \E[0m'
+#替换/etc/kubernetes中与IP地址关联的配置
+find . -type f | xargs sed -i "s/$o_ip/$n_ip/"
+if [ $? != 0 ];then
+    echo -e '\E[1;31m [x] 替换/etc/kubernetes中的IP失败 \E[0m'
     exit 0
 fi
-if [ ! -f "/etc/kubernetes/manifests/kube-apiserver.yaml" ];then
-    echo '\E[1;31m [x] /etc/kubernetes/manifests/kube-apiserver.yaml文件不存在 \E[0m'
-    exit 0
+
+#更新/etc/hosts的ip
+sed -i "s/$o_ip/$n_ip/g" /etc/hosts
+
+#重新生成 etcd-server 证书
+if [ -f "/etc/kubernetes/pki/etcd/server.crt" ];then
+    rm -rf /etc/kubernetes/pki/etcd/server.crt
 fi
-sed -i "s/$o_ip/$n_ip/g" /etc/kubernetes/manifests/etcd.yaml
-sed -i "s/$o_ip/$n_ip/g" /etc/kubernetes/manifests/kube-apiserver.yaml
-if [ $? == 0 ];then
-    echo -e '\E[1;32m [v] etcd.yaml、kube-apiserver.yaml替换ip成功 \E[0m'
+if [ -f "/etc/kubernetes/pki/etcd/server.key" ];then
+    rm -rf /etc/kubernetes/pki/etcd/server.key
+fi
+kubeadm init phase certs etcd-server
+if [ ! -f "/etc/kubernetes/pki/etcd/server.crt" ];then
+    echo -e '\E[1;31m [x] /etc/kubernetes/pki/etcd/server.crt生成失败 \E[0m'
+    exit 0
 else
-    echo -e '\E[1;31m [x] etcd.yaml、kube-apiserver.yaml替换ip失败 \E[0m'
+    echo -e '\E[1;32m [v] 生成/etc/kubernetes/pki/etcd/server.crt \E[0m'
+fi
+if [ ! -f "/etc/kubernetes/pki/etcd/server.key" ];then
+    echo -e '\E[1;32m [x] /etc/kubernetes/pki/etcd/server.key生成失败 \E[0m'
     exit 0
+else
+    echo -e '\E[1;32m [v] 生成/etc/kubernetes/pki/etcd/server.key \E[0m'
+fi
+
+#重新生成 apiserver 证书
+if [ -f "/etc/kubernetes/pki/apiserver.key" ];then
+    rm -rf /etc/kubernetes/pki/apiserver.key
+fi
+if [ -f "/etc/kubernetes/pki/apiserver.crt" ];then
+    rm -rf /etc/kubernetes/pki/apiserver.crt
+fi
+kubeadm init phase certs apiserver  --apiserver-advertise-address $n_ip
+if [ ! -f "/etc/kubernetes/pki/apiserver.key" ];then
+    echo -e '\E[1;31m [x] 生成/etc/kubernetes/pki/apiserver.key失败 \E[0m'
+    exit 0
+else
+    echo -e '\E[1;32m [v] 生成/etc/kubernetes/pki/apiserver.key \E[0m'
+fi
+if [ ! -f "/etc/kubernetes/pki/apiserver.crt" ];then
+    echo -e '\E[1;31m [x] 生成/etc/kubernetes/pki/apiserver.crt失败 \E[0m'
+    exit 0
+else
+    echo -e '\E[1;32m [v] 生成/etc/kubernetes/pki/apiserver.crt \E[0m'
+fi
+
+#重新生成 apiserver-kubelet-client 证书
+if [ -f "/etc/kubernetes/pki/apiserver-kubelet-client.key" ];then
+    rm -rf /etc/kubernetes/pki/apiserver-kubelet-client.key
+fi
+if [ -f "/etc/kubernetes/pki/apiserver-kubelet-client.crt" ];then
+    rm -rf /etc/kubernetes/pki/apiserver-kubelet-client.crt
+fi
+kubeadm init phase certs apiserver-kubelet-client
+if [ ! -f "/etc/kubernetes/pki/apiserver-kubelet-client.crt" ];then
+    echo -e '\E[1;31m [x] 生成/etc/kubernetes/pki/apiserver-kubelet-client.crt失败 \E[0m'
+    exit 0
+else
+    echo -e '\E[1;32m [v] 生成/etc/kubernetes/pki/apiserver-kubelet-client.crt \E[0m'
+fi
+if [ ! -f "/etc/kubernetes/pki/apiserver-kubelet-client.key" ];then
+    echo -e '\E[1;31m [x] 生成/etc/kubernetes/pki/apiserver-kubelet-client.key失败 \E[0m'
+    exit 0
+else
+    echo -e '\E[1;32m [v] 生成/etc/kubernetes/pki/apiserver-kubelet-client.key \E[0m'
+fi
+
+#重新生成 front-proxy-client 证书
+if [ -f "/etc/kubernetes/pki/front-proxy-client.crt" ];then
+    rm -rf /etc/kubernetes/pki/front-proxy-client.crt
+fi
+if [ -f "/etc/kubernetes/pki/front-proxy-client.key" ];then
+    rm -rf /etc/kubernetes/pki/front-proxy-client.key
+fi
+kubeadm init phase certs front-proxy-client
+if [ ! -f "/etc/kubernetes/pki/front-proxy-client.key" ];then
+    echo -e '\E[1;31m [x] 生成/etc/kubernetes/pki/front-proxy-client.key失败 \E[0m'
+    exit 0
+else
+    echo -e '\E[1;32m [v] 生成/etc/kubernetes/pki/front-proxy-client.key \E[0m'
+fi
+if [ ! -f "/etc/kubernetes/pki/front-proxy-client.crt" ];then
+    echo -e '\E[1;31m [x] 生成/etc/kubernetes/pki/front-proxy-client.crt失败 \E[0m'
+    exit 0
+else
+    echo -e '\E[1;32m [v] 生成/etc/kubernetes/pki/front-proxy-client.crt \E[0m'
 fi
 
 #生成新的config文件
@@ -37,30 +114,10 @@ if [ ! -f "/etc/kubernetes/admin.conf" ];then
 else
     echo -e '\E[1;32m [v] 生成新的/etc/kubernetes/admin.conf文件 \E[0m'
 fi
-#删除老证书，生成新证书
-if [ -f "/etc/kubernetes/pki/apiserver.key" ];then
-    rm -rf /etc/kubernetes/pki/apiserver.key
-fi
-if [ -f "/etc/kubernetes/pki/apiserver.crt" ];then
-    rm -rf /etc/kubernetes/pki/apiserver.crt
-fi
-kubeadm init phase certs apiserver  --apiserver-advertise-address $n_ip
-if [ ! -f "/etc/kubernetes/pki/apiserver.key" ];then
-    echo -e '\E[1;31m [x] 生成/etc/kubernetes/pki/apiserver.key失败 \E[0m'
-    exit 0
-else
-    echo -e '\E[1;32m [v] 生成新的/etc/kubernetes/pki/apiserver.key文件 \E[0m'
-fi
-if [ ! -f "/etc/kubernetes/pki/apiserver.crt" ];then
-    echo -e '\E[1;31m [x] 生成/etc/kubernetes/pki/apiserver.crt失败 \E[0m'
-    exit 0
-else
-    echo -e '\E[1;32m [v] 生成新的/etc/kubernetes/pki/apiserver.crt文件 \E[0m'
-fi
-#重启docker
-service docker restart
-service kubelet restart
 
+#重启docker\kubelet
+systemctl daemon-reload && systemctl restart docker && systemctl restart kubelet
+echo -e '\E[1;32m [v] 重启docker、kubelet \E[0m'
 #将配置文件config输出
 if [ ! -f "/etc/kubernetes/admin.conf" ];then
     echo '\E[1;31m [x] /etc/kubernetes/admin.conf文件不存在 \E[0m'
@@ -71,7 +128,7 @@ kubectl get nodes --kubeconfig=/etc/kubernetes/admin.conf
 mv /etc/kubernetes/admin.conf ~/.kube/config
 
 if [ $? == 0 ];then
-    echo -e '\E[1;31m success \E[0m'
+    echo -e '\E[1;32m success \E[0m'
     exit 0
 fi
 
